d709012f836b0ec66e69448060eeb74426539866
wxiaojia <630993632@qq.com>
2016-11-13 21:01:13 +0800

    11

--- Public/js/index1.js
+++ Public/js/index1.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-	$(".pic").on("click",function(e){
-		$("#hide").css({"z-index":" 10","display"})
-	})
+	  $("#hide").hide();
+       $(".pic").on("click",function(e){
+			var top = ($(window).height() - $("#hide").height())/2;   
+			var left = ($(window).width() - $("#hide").width())/2;   
+			var scrollTop = $(document).scrollTop();   
+			var scrollLeft = $(document).scrollLeft();   
+			$("#hide").css( {'top' : top + scrollTop, "left" : left + scrollLeft } ).show();
+       })
 })
